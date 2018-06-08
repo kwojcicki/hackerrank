814955e7f06f304236982f785ce32e2c4448786b
kwojcicki <kwojcicki@sympatico.ca>
2018-06-07 22:37:14 -0400

    lowest triangle

---
+++ solutions/src/solutions/mathematics/lowest_triangle/Solution.java
@@ -0,0 +1,22 @@
+package solutions.mathematics.lowest_triangle;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int lowestTriangle(int base, int area){
+        return (int)Math.ceil(area * 2.00 / base);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int base = in.nextInt();
+        int area = in.nextInt();
+        int height = lowestTriangle(base, area);
+        System.out.println(height);
+    }
+}
