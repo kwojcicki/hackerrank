2836ed725ec52c0fb1b22031e66e4b56d64e0fe6
kwojcicki <kwojcicki@sympatico.ca>
2018-06-10 17:44:49 -0400

    fixed imports

--- solutions/src/solutions/mathematics/lowest_triangle/Solution.java
+++ solutions/src/solutions/mathematics/lowest_triangle/Solution.java
@@ -1,10 +1,6 @@
 package solutions.mathematics.lowest_triangle;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class Solution {
 
