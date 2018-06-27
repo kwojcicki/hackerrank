d0d962a5e9e22e5b1c2c23957cbbb9b0d43c7fdd
kwojcicki <kwojcicki@sympatico.ca>
2018-06-26 20:47:18 -0400

    fixed

--- solutions/src/solutions/mathematics/filling_jars/Solution.java
+++ solutions/src/solutions/mathematics/filling_jars/Solution.java
@@ -7,50 +7,20 @@ import java.util.Scanner;
 
 public class Solution {
 
-    // Complete the solve function below.
-    static int solve(int n, int[][] operations) {
-    	long total = 0;
-    	long k = n;
-    	for(int i = 0; i < operations.length; i++) {
-    		long a = (long) operations[i][1];
-    		long b = (long) operations[i][0];
-    		long candies = (long) operations[i][2];
-    		total = total + (a - b + 1) * candies;
-    	}
-    	
-    	return (int)(total / k);
-    }
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		long M = sc.nextInt();
+		long N = sc.nextInt();
 
-    private static final Scanner scanner = new Scanner(System.in);
+		long sum=0;
+		for(int i=0;i<N;i++)
+		{
+			long a = sc.nextLong();
+			long b = sc.nextLong();
+			long k = sc.nextLong();
+			sum+=(b-a+1)*k;
+		}
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] nm = scanner.nextLine().split(" ");
-
-        int n = Integer.parseInt(nm[0]);
-
-        int m = Integer.parseInt(nm[1]);
-
-        int[][] operations = new int[m][3];
-
-        for (int operationsRowItr = 0; operationsRowItr < m; operationsRowItr++) {
-            String[] operationsRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int operationsColumnItr = 0; operationsColumnItr < 3; operationsColumnItr++) {
-                int operationsItem = Integer.parseInt(operationsRowItems[operationsColumnItr]);
-                operations[operationsRowItr][operationsColumnItr] = operationsItem;
-            }
-        }
-
-        int result = solve(n, operations);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
-    }
+		System.out.println(sum/M);
+	}
 }
